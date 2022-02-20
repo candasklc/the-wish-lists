191c23c8ff0d6f798023350c6e41189b4a84c7f9
Candaş Kılıç <63592580+candasklc@users.noreply.github.com>
2022-02-20 15:01:21 +0100

    revert changes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require("./config/db");
-const getAllWishes = require("./dbInteraction/getAllWishes");
-const addWish = require("./dbInteraction/addWish");
-const deleteWish = require("./dbInteraction/deleteWish");
+const ObjectId = require("mongoose").Types.ObjectId;
+const { collection } = require("./models/wish");
+// const getAllWishes = require("./dbInteraction/getAllWishes");
+// const addWish = require("./dbInteraction/addWish");
+// const deleteWish = require("./dbInteraction/deleteWish");
 
 app.use(express.json());
 
@@ -28,15 +30,42 @@ app.use(express.static(path.join(__dirname, "/src/app/components/mainpage")));
 
 // ------------- Db interaction
 app.get("/all", (req, res) => {
-  getAllWishes(req, res);
+  collection.find({}).toArray((err, results) => {
+    if (!err) {
+      res.send(results);
+    } else {
+      res.status(err);
+    }
+  });
 });
 
 app.post("/add-wish", (req, res) => {
-  addWish(req, res);
+  const newWish = {
+    title: req.body.title,
+    link: req.body.link,
+    user: req.body.user,
+  };
+  collection.insertOne(newWish, (err, result) => {
+    if (!err) {
+      res.json(result);
+    } else {
+      res.json("Error was raised while adding an item. " + err);
+    }
+  });
 });
 
 app.delete("/delete/:id", (req, res) => {
-  deleteWish(req, res);
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    res.json("No records with given id: " + id);
+  } else {
+    try {
+      collection.deleteOne({ _id: ObjectId(id) });
+      res.json("Item is removed with id: " + id);
+    } catch (e) {
+      res.json("Error was raised while removing an item. " + e);
+    }
+  }
 });
 
 app.listen(PORT, () => {
